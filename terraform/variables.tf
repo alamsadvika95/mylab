23ddcea0468ae20403df84c249e069f802e3b7bb
alamsadvika95 <alamsadvika@hotmail.com>
2022-03-09 07:15:57 +0700

    testing

---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "project_id" {
+ description = "The project ID to host the cluster in"
+ default   ="terraform-343304"
+}
+variable "cluster_name" {
+ description = "The name for the GKE cluster"
+ default     = "testing"
+}
+variable "location" {
+ description = "The region to host the cluster in"
+ default     = "us-central1"
+}
+variable "machine_type" {
+ description = "The region to host the cluster in"
+ default     = "e2-micro"
+}
+
+
