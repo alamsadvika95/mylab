17c565260b1cf0d7f71b5ddf466d00db5d2bc5fe
alamsadvika95 <alamsadvika@hotmail.com>
2022-03-10 10:41:21 +0700

    testing cloudsql

--- terraform/module/database/cloudsql/main.tf
+++ terraform/module/database/cloudsql/main.tf
@@ -20,7 +20,7 @@ resource "google_sql_database_instance" "read_replica" {
       private_network = "projects/${var.project_id}/global/networks/default"
     }
     location_preference {
-      zone = "europe-west4-a"
+      zone = "${var.region}"
     }
   }
 }
