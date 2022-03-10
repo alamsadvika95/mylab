a5d544b95f71eb4e3713a46aa8af6a924a3ea229
alamsadvika95 <alamsadvika@hotmail.com>
2022-03-10 10:38:24 +0700

    testing cloudsql

--- terraform/module/database/cloudsql/variables.tf
+++ terraform/module/database/cloudsql/variables.tf
@@ -1,6 +1,6 @@
 variable "project_id" {
   description = "The project ID to host the cluster in"
-  default     = "cicd-339902" 
+  default     = "terraform-343304" 
 }
 variable "master_instance" {
   description = "The name for the GKE cluster"
