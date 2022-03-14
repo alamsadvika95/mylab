8554821ad404a3dea566b5a2b4327f6ad325a77a
alamsadvika95 <alamsadvika@hotmail.com>
2022-03-14 10:44:59 +0700

    testing infra 99

--- terraform/lab/99-infra/cloudsql/main.tf
+++ terraform/lab/99-infra/cloudsql/main.tf
@@ -1,4 +1,4 @@
-resource "google_sql_database_instance" "read_replica" {
+resource "google_sql_database_instance" "cloudsql_instance" {
   name                 = "${var.master_instance}"
   region               = "${var.region}"
   database_version     = "MYSQL_5_7"
