9fc0de5d78d4b97180caec7fb31daf558ae6cede
alamsadvika95 <alamsadvika@hotmail.com>
2022-03-22 17:46:39 +0700

    testing

--- app/nodejs/index.js
+++ app/nodejs/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const userRoute = require("./routes/user");
-const authRoute = require("./routes/auth");
-const productRoute = require("./routes/product");
-const orderRoute = require("./routes/order");
-const cartRoute = require("./routes/cart");
-const stripeRoute = require("./routes/stripe");
-const cors = require("cors");
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
+// const userRoute = require("./routes/user");
+// const authRoute = require("./routes/auth");
+// const productRoute = require("./routes/product");
+// const orderRoute = require("./routes/order");
+// const cartRoute = require("./routes/cart");
+// const stripeRoute = require("./routes/stripe");
+// const cors = require("cors");
 //coba node-test
 const bodyParser = require('body-parser');
 const multer = require('multer');
@@ -18,24 +18,24 @@ const User = require("./config/User");
 //Configuration for .env
 dotenv.config();
 
-//Connect to MongoDB
-mongoose.connect(
-  process.env.MONGO_URI)
-  .then(() => console.log("DBConnection Successful"))
-  .catch((err) => {
-      console.log(err);
-  });
+// //Connect to MongoDB
+// mongoose.connect(
+//   process.env.MONGO_URI)
+//   .then(() => console.log("DBConnection Successful"))
+//   .catch((err) => {
+//       console.log(err);
+//   });
 
-//Cors
-app.use(cors());
-//Routes
-app.use(express.json());
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/products", productRoute);
-app.use("/api/orders", orderRoute);
-app.use("/api/carts", cartRoute);
-app.use("/api/checkout", stripeRoute);
+// //Cors
+// app.use(cors());
+// //Routes
+// app.use(express.json());
+// app.use("/api/auth", authRoute);
+// app.use("/api/users", userRoute);
+// app.use("/api/products", productRoute);
+// app.use("/api/orders", orderRoute);
+// app.use("/api/carts", cartRoute);
+// app.use("/api/checkout", stripeRoute);
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
